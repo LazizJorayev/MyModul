ea8d9758d3dffcfddf07ff64e501f05854a96675
pc <pc@DESKTOP-8SL5GCF>
2025-03-10 20:47:46 +0500

    Add project files.

---
+++ Modul2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Misol_1
+double r, s, l;
+Console.WriteLine("R = ");
+r = int.Parse(Console.ReadLine());
+s = Math.PI * r * r;
+l = 2 * Math.PI * r;
+Console.WriteLine($"S = {s} l = {l}");
+//Misol_2
+int Kurs = 12500, a;
+Console.WriteLine("Summa($) = ");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}$ = {a*Kurs}");
+//Misol_3
+int yosh;
+Console.WriteLine("Yoshni kiriting = ");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Kun = {(2025 - a)*365}");
